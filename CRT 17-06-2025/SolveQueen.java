60a4695b41b0430716ad6cc419d86f47247e49ae
hareesh211 <hareeshkoyalakunta@gmail.com>
2025-06-17 12:12:48 +0530

    update

--- CRT 17-06-2025/SolveQueen.java
+++ CRT 17-06-2025/SolveQueen.java
@@ -1,5 +1,20 @@
 class SolveQueen{
     public static void main(String args[]){
-
+int[][]board={
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0},
+};
+solveQueen(board,0);
+    }
+    public static void solveQueen(int[][]board,int row){
+        if(row==board.length){
+            printBoard(board);
+        }
     }
 }
